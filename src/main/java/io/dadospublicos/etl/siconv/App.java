0a618bad9d16296d52c0ea5b4f35cb40425de1d3
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-09-11 20:52:43 -0300

    Adicionando propriedades do producer

--- src/main/java/io/dadospublicos/etl/siconv/App.java
+++ src/main/java/io/dadospublicos/etl/siconv/App.java
@@ -1,11 +1,27 @@
 package io.dadospublicos.etl.siconv;
 
+import java.util.Properties;
+import org.apache.kafka.common.serialization.StringSerializer;
+
 /**
  * ETL Siconv
  *
  */
 public class App {
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("ETL Siconv");
+
+        String bootstrapServers = "192.168.0.101:9092";
+
+        // Propriedades do producer
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", bootstrapServers);
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+        // Criação do producer
+
+        // Carregamento dos dados
+
     }
 }
