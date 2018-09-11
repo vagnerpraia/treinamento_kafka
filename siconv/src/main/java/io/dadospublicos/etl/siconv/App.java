847969cde9de41e65e83d802051180c8293b7d1c
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-09-11 17:46:58 -0300

    Estruturando projeto

---
+++ siconv/src/main/java/io/dadospublicos/etl/siconv/App.java
@@ -0,0 +1,13 @@
+package io.dadospublicos.etl.siconv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
