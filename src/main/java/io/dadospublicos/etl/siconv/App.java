e7f024b9ca606c3e021091a0e9f79d372e849bf5
Vagner Praia da Silva <vagnerpraia@gmail.com>
2018-09-12 01:21:02 -0300

    Producer de teste

--- src/main/java/io/dadospublicos/etl/siconv/App.java
+++ src/main/java/io/dadospublicos/etl/siconv/App.java
@@ -1,7 +1,10 @@
 package io.dadospublicos.etl.siconv;
 
 import java.util.Properties;
+
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
 /**
@@ -12,7 +15,7 @@ public class App {
     public static void main(String[] args){
         System.out.println("ETL Siconv");
 
-        String bootstrapServers = "192.168.0.101:9092";
+        String bootstrapServers = "127.0.0.1:9092";
 
         // Propriedades do producer
         Properties properties = new Properties();
@@ -21,8 +24,16 @@ public class App {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // Criação do producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // Criação do registros do producer
+        ProducerRecord<String, String> record = new ProducerRecord<String, String>("siconv", "ola mundo");
 
         // Carregamento dos dados
+        producer.send(record);
 
+        // Forçar o carregamento dos dados no cunsumer
+        producer.flush();
+        producer.close();
     }
 }
